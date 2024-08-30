1b9d8f9f4126987430e3ae8af2f039204cacc593
john-erik-moscoso <john-erik@outlook.es>
2024-08-30 01:20:53 -0500

    Android setup

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,8 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
+import 'firebase_options.dart';
+
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
